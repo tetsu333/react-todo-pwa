9802fed0c8e5b02acc2d97b9e82903f388be97a0
tetsu <cda07c5117@gmail.com>
2022-07-25 00:00:49 +0900

    ログイン機能実装

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import dig from "object-dig"
+import { signInWithGoogle, logOut } from "../service/firebase";
+import { AuthContext } from "../providers/AuthProvider";
+
+const Header = () => {
+  const currentUser = useContext(AuthContext);
+  console.log(currentUser);
+  const buttonRender = () => {
+    let buttonDom;
+    if (dig(currentUser, 'currentUser')) {
+      buttonDom = <button onClick={logOut}>ログアウト</button>;
+    } else {
+      buttonDom = <button onClick={signInWithGoogle}>ログイン</button>;
+    }
+    return buttonDom;
+  };
+
+  return (
+    <header>
+      ヘッダー
+      {buttonRender()}
+    </header>
+  );
+};
+
+export default Header;
